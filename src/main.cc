7e698d70491c17daa3a07b035087c8fd7c6d8ada
Justin Tervay <jtervay@theaiinstitute.com>
2023-09-13 13:50:58 -0400

    Add quartiles

--- src/main.cc
+++ src/main.cc
@@ -9,6 +9,40 @@
 #include "src/benchmark_ros2.h"
 #include "src/benchmark_upb.h"
 
+double calculateQ1(const std::vector<double> &v) {
+  std::vector<double> sorted = v;
+  std::sort(sorted.begin(), sorted.end());
+
+  size_t n = sorted.size();
+  size_t q1_index = n / 4;
+  double q1 = 0.0;
+
+  if (n % 4 == 0) {
+    q1 = (sorted[q1_index - 1] + sorted[q1_index]) / 2.0;
+  } else {
+    q1 = sorted[q1_index];
+  }
+
+  return q1;
+}
+
+double calculateQ3(const std::vector<double> &v) {
+  std::vector<double> sorted = v;
+  std::sort(sorted.begin(), sorted.end());
+
+  size_t n = sorted.size();
+  size_t q3_index = 3 * n / 4;
+  double q3 = 0.0;
+
+  if (n % 4 == 0) {
+    q3 = (sorted[q3_index - 1] + sorted[q3_index]) / 2.0;
+  } else {
+    q3 = sorted[q3_index];
+  }
+
+  return q3;
+}
+
 inline constexpr size_t num_cycle_tests[] = {1, 10, 100};
 
 static void CustomArguments(benchmark::internal::Benchmark *b) {
@@ -36,6 +70,14 @@ static void CustomArguments(benchmark::internal::Benchmark *b) {
                           [](const std::vector<double> &v) -> double {         \
                             return *(                                          \
                                 std::min_element(std::begin(v), std::end(v))); \
+                          })                                                   \
+      ->ComputeStatistics("q1",                                                \
+                          [](const std::vector<double> &v) -> double {         \
+                            return calculateQ1(v);                             \
+                          })                                                   \
+      ->ComputeStatistics("q3",                                                \
+                          [](const std::vector<double> &v) -> double {         \
+                            return calculateQ3(v);                             \
                           })                                                   \
       ->Apply(CustomArguments);
 
